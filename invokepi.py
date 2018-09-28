8ce7c33f78e82b17f2cf1b6993b5a4b5bc371f77
Brandon <btgrant6@gmail.com>
2018-09-28 00:31:25 -0500

    Finalized

--- invokepi.py
+++ invokepi.py
@@ -1,9 +1,5 @@
 from pi import *
-print('What is your first name?')
-first = input()
-print('What is your last name?')
-last = input()
-name = Pi(first, last)
-print('Your full name is ' + name.fullname)
-print('Your first name is ' + name.first)
-print('Your last name is ' + name.last)
+print('Enter the sample size of points: ')
+sample = int(input())
+init = Pi(sample)
+print('Approximation of pi = ' + init.pi.__str__())
--- pi.py
+++ pi.py
@@ -1,20 +1,48 @@
+# authors: Brandon Grant, Dylan Hensley, Ethan Twilley
+# date: 10/3/2018
+# description: program approximates pi
+
+import random as point
+
+
 class Pi:
-    def __init__(self, name, last):
-        self.first = name
-        self.last = last
 
+    within = 0
+    pi = 0
+
+    # constructor initializes sample size, iterates x and y, and calculates pi
+    def __init__(self, sample):
+        self.sample = sample
+
+        for _ in range(sample):
+            self.x = point.random() ** 2
+            self.y = point.random() ** 2
+            self.is_within()
+
+        self.pi = 4 * self.within / sample
+
+    # getter function for x
     @property
-    def fullname(self):
-        return "%s %s" % (self.first, self.last)
+    def x(self):
+        return self.__x
 
-    @fullname.setter
-    def fullname(self, value):
-        # this is much more complicated in real life
-        first, last = value.split(" ", 1)
-        self.first = first
-        self.last = last
+    # getter function for y
+    @property
+    def y(self):
+        return self.__y
 
-    @fullname.deleter
-    def fullname(self):
-        del self.first
-        del self.last
+    # setter function for x
+    @x.setter
+    def x(self, points):
+        self.__x = points
+
+    # setter function for y
+    @y.setter
+    def y(self, points):
+        self.__y = points
+
+    # function determines if x and y points are within circle
+    def is_within(self):
+        if self.__x + self.__y <= 1:
+            self.within += 1
+        return self.within
