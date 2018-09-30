1cb511457b0d0d958492f1919006827c3d746e69
Brandon <btgrant6@gmail.com>
2018-09-30 12:19:38 -0500

    Finalized

--- invokepi.py
+++ invokepi.py
@@ -1,3 +1,7 @@
+# authors: Brandon Grant, Dylan Hensley, Ethan Twilley
+# date: 10/3/2018
+# description: program approximates pi
+
 from pi import *
 print('Enter the sample size of points: ')
 sample = int(input())
--- pi.py
+++ pi.py
@@ -2,21 +2,19 @@
 # date: 10/3/2018
 # description: program approximates pi
 
-import random as point
+import random as r
 
 
 class Pi:
 
-    within = 0
-    pi = 0
+    within, pi = 0, 0
 
     # constructor initializes sample size, iterates x and y, and calculates pi
     def __init__(self, sample):
-        self.sample = sample
 
         for _ in range(sample):
-            self.x = point.random() ** 2
-            self.y = point.random() ** 2
+            self.x = r.random() ** 2
+            self.y = r.random() ** 2
             self.is_within()
 
         self.pi = 4 * self.within / sample
@@ -26,23 +24,23 @@ class Pi:
     def x(self):
         return self.__x
 
+    # setter function for x
+    @x.setter
+    def x(self, point):
+        self.__x = point
+
     # getter function for y
     @property
     def y(self):
         return self.__y
 
-    # setter function for x
-    @x.setter
-    def x(self, points):
-        self.__x = points
-
     # setter function for y
     @y.setter
-    def y(self, points):
-        self.__y = points
+    def y(self, point):
+        self.__y = point
 
     # function determines if x and y points are within circle
     def is_within(self):
         if self.__x + self.__y <= 1:
             self.within += 1
-        return self.within
+        return
